e1e41c4d349fb26e1f618ee14acd9578868f1b6d
namds29 <namds2920@gmail.com>
2024-01-17 00:47:34 +0700

    add page about

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,26 +1,26 @@
 import React, { useState } from "react";
 import styles from "./layout.module.scss";
 import Header from "./header/header";
+import Footer from "./footer";
 
 
 type Props = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 const Layout: React.FC<Props> = ({ children }) => {
-    const [menuOpen, setMenuOpen] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(true);
 
-    const handleMenuToggle = () => {
-        setMenuOpen(!menuOpen);
-    };
-    return (
-        <div>
-            <Header handleMenuToggle={handleMenuToggle} />
-            <div className={styles.container}>
-                <div className={styles["main-content"]}>
-                    {children}
-                </div>
-            </div>
-        </div>
-    );
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+  return (
+    <div>
+      <Header handleMenuToggle={handleMenuToggle} />
+      <div className={styles.container}>
+        <div className={styles["main-content"]}>{children}</div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 export default Layout;
