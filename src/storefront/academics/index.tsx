3942458381e88867a399b8732f681ae6bb1c0d44
Nam Doan <namds2920@gmail.com>
2024-01-28 14:11:53 +0700

    addition

--- src/storefront/academics/index.tsx
+++ src/storefront/academics/index.tsx
@@ -1,12 +1,18 @@
-import { ACADEMICS_FUCTIONAL_UNITS } from "src/utils/constant";
+import { ACADEMICS_FUCTIONAL_UNITS, ACADEMICS_TRAINING_PROGRAM } from "src/utils/constant";
 import styles from "./index.module.scss";
 import { useState } from "react";
 
 const Academics = () => {
   const [unitId, setUnitId] = useState<number>(1);
+  const [trainingTitle, setTrainingTitle] = useState<string>('');
+
   const handleChangeUnit = (id: number) => {
     console.log(id);
     setUnitId(id);
+  };
+  const handleChangeTitle = (title: string) => {
+    console.log(title);
+    setTrainingTitle(title);
   };
 
   return (
@@ -20,43 +26,85 @@ const Academics = () => {
       </div>
       <section className="bg-gray-100 w-full text-orange-500 font-bold  justify-between items-center px-20 py-8">
         <p className="text-3xl border-b-4 text-center">Academics</p>
-        <p className="text-2xl mt-8">Functional Units</p>
-        <div className="flex border text-black border-black mt-4">
-          <div className="title w-1/3">
-            {ACADEMICS_FUCTIONAL_UNITS.map((item) => (
-              <div
-                onClick={() => handleChangeUnit(item.id)}
-                className="cursor-pointer text-blue-800 hover:text-blue-700 px-4 py-2 border-r border-black"
-              >
-                <a>{item.title}</a>
-              </div>
-            ))}
+        <section id="functional_unit">
+          <p className="text-2xl mt-8">Functional Units</p>
+          <div className="flex border text-black border-black mt-4">
+            <div className="title w-1/3">
+              {ACADEMICS_FUCTIONAL_UNITS.map((item) => (
+                <div
+                  onClick={() => handleChangeUnit(item.id)}
+                  className="cursor-pointer text-blue-800 hover:text-blue-700 px-4 py-2 border-r border-black"
+                >
+                  <a>{item.title}</a>
+                </div>
+              ))}
+            </div>
+            <div className="description font-normal p-4 w-2/3">
+              {unitId == 1 && (
+                <div className="content">
+                  <p className="font-bold">1. General administrative office</p>
+                  <p>Introduction:</p>
+                  <p>Contact:</p>
+                  <p className="font-bold">2. Organization and staff</p>
+                  <p>Introduction:</p>
+                  <p>Contact:</p>
+                  <p>(...)</p>
+                </div>
+              )}
+              {unitId == 2 && (
+                <div className="content">
+                  <p className="font-bold">1. Organization and staff</p>
+                  <p>Introduction:</p>
+
+                  <p className="font-bold">2. Organization and staff</p>
+                  <p>Introduction:</p>
+                  <p>Contact:</p>
+                  <p>(...)</p>
+                </div>
+              )}
+            </div>
           </div>
-          <div className="description font-normal p-4 w-2/3">
-            {unitId == 1 && (
-              <div className="content">
-                <p className="font-bold">1. General administrative office</p>
-                <p>Introduction:</p>
-                <p>Contact:</p>
-                <p className="font-bold">2. Organization and staff</p>
-                <p>Introduction:</p>
-                <p>Contact:</p>
-                <p>(...)</p>
-              </div>
-            )}
-            {unitId == 2 && (
-              <div className="content">
-                <p className="font-bold">1. Organization and staff</p>
-                <p>Introduction:</p>
-                
-                <p className="font-bold">2. Organization and staff</p>
-                <p>Introduction:</p>
-                <p>Contact:</p>
-                <p>(...)</p>
-              </div>
-            )}
+        </section>
+        <section id="functional_unit">
+          <p className="text-2xl mt-8">Training Program</p>
+          <div className="flex border text-black border-black mt-4">
+            <div className="title w-1/3">
+              {ACADEMICS_TRAINING_PROGRAM.map((item) => (
+                <div
+                 
+                  className="cursor-pointer text-black px-4 py-2 border-r border-black"
+                >
+                  <a>{item.parent_title}</a>
+                  <div className="ml-4 text-blue-800 hover:text-blue-700">a</div>
+                </div>
+              ))}
+            </div>
+            {/* <div className="description font-normal p-4 w-2/3">
+              {unitId == 1 && (
+                <div className="content">
+                  <p className="font-bold">1. General administrative office</p>
+                  <p>Introduction:</p>
+                  <p>Contact:</p>
+                  <p className="font-bold">2. Organization and staff</p>
+                  <p>Introduction:</p>
+                  <p>Contact:</p>
+                  <p>(...)</p>
+                </div>
+              )}
+              {unitId == 2 && (
+                <div className="content">
+                  <p className="font-bold">1. Organization and staff</p>
+                  <p>Introduction:</p>
+
+                  <p className="font-bold">2. Organization and staff</p>
+                  <p>Introduction:</p>
+                  <p>Contact:</p>
+                  <p>(...)</p>
+                </div>
+              )}
+            </div> */}
           </div>
-        </div>
+        </section>
       </section>
     </section>
   );
