ee411ee463c0bef1c03195d09f48ed15789e7de5
Tran Duc Anh <dtran3565@gmail.com>
2024-03-14 00:05:14 +0700

    Coding

--- src/storefront/student-support/index.tsx
+++ src/storefront/student-support/index.tsx
@@ -61,8 +61,8 @@ const StudentSupport = () => {
       <Banner />
 
       <section className="bg-gray-100 w-full px-24 py-6">
-        <div className="w-full flex justify-center mt-6 mb-10">
-          <p className="text-5xl text-black">International Student Guidebook</p>
+        <div className="w-full flex justify-center mt-6 mb-10 border-b-4">
+          <p className="text-5xl text-orange-500 ">International Student Guidebook</p>
         </div>
 
         <div className="flex text-orange-500 font-bold items-center">
@@ -76,7 +76,7 @@ const StudentSupport = () => {
                 className="m-10 relative flex items-center justify-center"
               >
                 <img
-                  className="object-cover w-full h-full rounded-lg"
+                  className="object-cover w-full h-full rounded-lg filter brightness-50"
                   src="/img/img1.png"
                   alt=""
                 />
@@ -92,18 +92,18 @@ const StudentSupport = () => {
       <Banner />
 
       <section className="bg-gray-100 w-full px-24 py-6">
-        <div className="flex text-orange-500 font-bold items-center">
+        <div className="flex text-orange-500 font-bold items-center mb-6">
           <p className="text-xl border-b-4">STUDENT SERVICES</p>
         </div>
-        <div className="grid grid-cols-2 gap-4 w-full">
+        <div className="flex gap-4 w-full">
           {serviceData.map((item) => {
             return (
               <div
                 key={item.id}
-                className="mx-10 relative flex items-center justify-center"
+                className="mx-10 relative flex items-center justify-center w-full h-80"
               >
                 <img
-                  className="object-cover w-full h-4/6 rounded-lg "
+                  className="w-full object-cover rounded-lg filter brightness-50 h-full"
                   src="/img/img1.png"
                   alt=""
                 />
@@ -117,28 +117,27 @@ const StudentSupport = () => {
       </section>
 
       <section className="bg-gray-100 w-full px-24 py-8">
-        <div className="flex text-black font-bold mb-6 justify-between items-center">
-          <p className="text-4xl border-b-4">RECRUITMENT</p>
+        <div className="flex text-orange-500 font-bold mb-6 justify-between items-center">
+          <p className="text-xl border-b-4">RECRUITMENT</p>
         </div>
         <div className="mx-10">
-
-        {recruitmentData.map((item) => {
-          return (
-            <Card key={item.id} className="my-2">
-              <div className="flex flex-row">
-                <img
-                  src="/img/img1.png"
-                  className="w-1/4 rounded mr-6"
-                  alt=""
-                />
-                <div className="flex flex-col justify-center ">
-                  <p className="text-3xl font-bold">{item.title}</p>
-                  <p className="text-2xl font-medium">{item.subtitle}</p>
+          {recruitmentData.map((item) => {
+            return (
+              <Card key={item.id} className="my-2">
+                <div className="flex flex-row">
+                  <img
+                    src="/img/img1.png"
+                    className="w-1/4 rounded mr-6 "
+                    alt=""
+                  />
+                  <div className="flex flex-col justify-center ">
+                    <p className="text-3xl font-bold">{item.title}</p>
+                    <p className="text-2xl font-medium">{item.subtitle}</p>
+                  </div>
                 </div>
-              </div>
-            </Card>
-          );
-        })}
+              </Card>
+            );
+          })}
         </div>
       </section>
     </div>
